0920eb95a24ffc3b5c20669fa8b773d9fd9f41ea
문태호(Mun TaeHo) <mun05170@naver.com>
2022-08-07 16:54:59 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Canvas } from "@react-three/fiber";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <sphereGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
